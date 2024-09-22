071af923c31735565f25b326197c11bc52a1471a
Slava0135 <super.novalskiy_0135@inbox.ru>
2024-09-22 16:26:22 +0300

    libafl setup

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,130 @@
-use std::process::Command;
-use serde_json::{Result, Value};
+use std::borrow::Cow;
+
+use libafl::{prelude::*, state::State};
+use libafl_bolts::{
+    current_nanos, rands::StdRand, tuples::{tuple_list, Handle, Handled, MatchNameRef}, Named
+};
+use serde::Deserialize;
+use serde_json::Value;
+
+#[derive(Deserialize)]
+struct Output {
+    status: String,
+    errmsg: String,
+    estack: Value,
+}
+
+#[derive(Clone)]
+struct DiffStdOutObjective {
+    neogo_stdout_observer: Handle<StdOutObserver>,
+    neosharp_stdout_observer: Handle<StdOutObserver>,
+}
+
+impl<S> Feedback<S> for DiffStdOutObjective
+where
+    S: State,
+{
+    fn is_interesting<EM, OT>(
+        &mut self,
+        _state: &mut S,
+        _manager: &mut EM,
+        _input: &<S>::Input,
+        observers: &OT,
+        _exit_kind: &ExitKind,
+    ) -> Result<bool, Error>
+    where
+        EM: EventFirer<State = S>,
+        OT: ObserversTuple<S>,
+    {
+        let neogo_output = observers
+            .get(&self.neogo_stdout_observer)
+            .unwrap()
+            .stdout
+            .clone();
+        let neosharp_output = observers
+            .get(&self.neosharp_stdout_observer)
+            .unwrap()
+            .stdout
+            .clone();
+        match (neogo_output, neosharp_output) {
+            (Some(neogo_output), Some(neosharp_output)) => {
+                let neogo_output: Output = serde_json::from_slice(&neogo_output)
+                    .expect("failed to read json output from 'neo-go'");
+                let neosharp_output: Output = serde_json::from_slice(&neosharp_output)
+                    .expect("failed to read json output from 'neo-sharp'");
+                if neogo_output.status != neosharp_output.status {
+                    return Ok(true)
+                }
+                if neogo_output.estack != neosharp_output.estack {
+                    return Ok(true)
+                }
+                Ok(false)
+            }
+            _ => panic!("no output found"),
+        }
+    }
+}
+
+impl Named for DiffStdOutObjective {
+    fn name(&self) -> &Cow<'static, str> {
+        &Cow::Borrowed("DiffStdOutObjective")
+    }
+}
 
 fn main() {
-    let input = "DAxIZWxsbyB3b3JsZCE=";
-    let neo_go_output = Command::new("./harness/neo-go")
-        .args([input])
-        .output()
-        .expect("failed to execute process");
-    let neo_sharp_output = Command::new("./harness/neo-sharp")
-        .args([input])
-        .output()
-        .expect("failed to execute process");
+    let neogo_stdout_observer = StdOutObserver::new("neogo-stdout-observer");
+    let neosharp_stdout_observer = StdOutObserver::new("neosharp-stdout-observer");
 
-    let neo_go_output = String::from_utf8(neo_go_output.stdout).unwrap();
-    let neo_sharp_output = String::from_utf8(neo_sharp_output.stdout).unwrap();
-    println!("NeoGo output: {}", neo_go_output);
-    println!("NeoSharp output: {}", neo_sharp_output);
+    let mut objective = DiffStdOutObjective{
+        neogo_stdout_observer: neogo_stdout_observer.handle(),
+        neosharp_stdout_observer: neosharp_stdout_observer.handle(),
+    };
 
-    let neo_go_value: Value = serde_json::from_str(&neo_go_output).unwrap();
-    let neo_sharp_value: Value = serde_json::from_str(&neo_sharp_output).unwrap();
-    if neo_go_value == neo_sharp_value {
-        println!("-- outputs are equal --");
-    } else {
-        println!("-- outputs are different --")
-    }
+    let mut feedback = ();
+
+    let neogo_executor = CommandExecutor::builder()
+        .program("./harness/neo-go")
+        .arg_input_arg()
+        .stdout_observer(neogo_stdout_observer.handle())
+        .build(tuple_list!(neogo_stdout_observer))
+        .unwrap();
+
+    let neosharp_executor = CommandExecutor::builder()
+        .program("./harness/neo-sharp")
+        .arg_input_arg()
+        .stdout_observer(neosharp_stdout_observer.handle())
+        .build(tuple_list!(neosharp_stdout_observer))
+        .unwrap();
+
+    let mut executor = DiffExecutor::new(neogo_executor, neosharp_executor, ()); 
+
+    let mut state = StdState::new(
+        StdRand::with_seed(current_nanos()),
+        InMemoryCorpus::new(),
+        InMemoryCorpus::new(),
+        &mut feedback,
+        &mut objective,
+    )
+    .unwrap();
+
+    state
+        .corpus_mut()
+        .add(Testcase::new(BytesInput::from(
+            "DAxIZWxsbyB3b3JsZCE=".as_bytes().to_vec(),
+        )))
+        .unwrap();
+
+    let scheduler = QueueScheduler::new();
+    let mut fuzzer = StdFuzzer::new(scheduler, feedback, objective);
+    let mut manager = NopEventManager::new();
+    
+    let mut stages = tuple_list!(StdMutationalStage::new(NopMutator::new(
+        MutationResult::Mutated
+    )));
+
+    let corpus_id = fuzzer
+       .fuzz_one(&mut stages, &mut executor, &mut state, &mut manager)
+       .unwrap();
+
+    println!("last corpus: {}", corpus_id.0)
 }
